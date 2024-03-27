31405e0c8778fbb76b92aa58b36975bc298f5450
Porcelain <ai.red.porcelain@gmail.com>
2024-03-27 22:42:40 +0800

    加入專案檔案。

---
+++ EFCore/Models/EFCoreContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Models
+{
+	public class EFCoreContext : DbContext
+	{
+		private const string connectString = "Server=localhost;Database=efcore_test;User Id=sa;Password=s55660513;TrustServerCertificate=True;";
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(connectString);
+		}
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
---
+++ EFCore/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+//Console.WriteLine("start insert");
+//Console.WriteLine("start read");
+Console.WriteLine("start update");
+updateProduct(1);
+updateProduct(2);
+
+
+static void insertdata()
+{
+	using (var db = new EFCoreContext())
+	{
+		//insert data
+		Product product = new Product();
+		product.Name = "Casey Yang";
+		db.Add(product);
+
+		var product1 = new Product();
+		product1.Name = "Dong Yang";
+		db.Add(product1);
+
+		db.SaveChanges();
+	}
+}
+
+
+
+static void readdata()
+{
+	using (var db = new EFCoreContext())
+	{
+
+		List<Product> products = db.Products.ToList();
+		foreach (var p in products)
+		{
+			Console.WriteLine("{0} {1}", p.Id, p.Name);
+		}
+
+	}
+}
+
+static void updateProduct(int id)
+{
+	using (var db = new EFCoreContext())
+	{
+		Product product = db.Products.FirstOrDefault(p => p.Id == id);
+		if (product != null)
+		{
+			// Update the product's name
+			product.Name = "Better Pen Drive" + id.ToString() + id.ToString();
+
+			// Save changes to the database
+			db.SaveChanges();
+		}
+	}
+	return;
+}
+
+
+
